409cef1a48ff5e0681b325f3fe2bccd0e7533618
MiniFid <nathan.d.fidler@gmail.com>
2022-04-18 07:46:29 -0600

    first commit

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,8 @@
+var my_string = '\\', string2 = 'hello', sum;
+// adds 5 and 4 and assigns the total to the variable sum
+function add() {
+    sum = 5+4;
+    return sum
+}
+document.write(add());
+window.alert(my_string + string2);
