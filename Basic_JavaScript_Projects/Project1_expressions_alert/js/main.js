afd1aa5a39ac0c8a59ae7786330909f984979ff3
MiniFid <nathan.d.fidler@gmail.com>
2022-04-18 08:03:29 -0600

    added fontcolor

--- Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/js/main.js
@@ -1,8 +1,8 @@
-var my_string = '\\', string2 = 'hello', sum;
+var my_string = 'im deprecated and no longer supported', string2 = ' \\', sum;
 // adds 5 and 4 and assigns the total to the variable sum
 function add() {
     sum = 5+4;
     return sum
 }
-document.write(add());
-window.alert(my_string + string2);
+window.alert(add());
+document.write(my_string.fontcolor('red') + string2);
