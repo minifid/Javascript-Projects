07e5197a1e27c91e3bed69f46b8cf4eaa3e8ac5e
MiniFid <nathan.d.fidler@gmail.com>
2022-04-19 15:19:36 -0600

    Boolean/coercion

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,6 @@
+var myVar=10, inf=2E310, negInf=-2E310;
+document.getElementById("editMe").innerHTML = typeof myVar + " Coercion " + inf + ' ' + negInf;
+document.write(!(5>10), ' ', (5>10 && 7<10), ' ', (5>10 || 7<10), ' ', (5=='5'), ' ', (5==='5'))
+function my_function(){
+    
+}
