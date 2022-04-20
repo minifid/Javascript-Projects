0d5617b6a571dcd87dd83092b492382fe1ed5bc6
MiniFid <nathan.d.fidler@gmail.com>
2022-04-20 10:39:13 -0600

    combined an int and a string

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,5 +1,5 @@
 var myVar=10, inf=2E310, negInf=-2E310;
-document.getElementById("editMe").innerHTML = typeof myVar + " Coercion " + inf + ' ' + negInf;
+document.getElementById("editMe").innerHTML = typeof myVar + myvar + " Coercion " + inf + ' ' + negInf;
 document.write(!(5>10), ' ', (5>10 && 7<10), ' ', (5>10 || 7<10), ' ', (5=='5'), ' ', (5==='5'))
 function my_function(){
     
